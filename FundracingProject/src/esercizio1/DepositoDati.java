d4a9cc2986f5403495f8eab6116534e3006c2416
Lucia367 <56352430+Lucia367@users.noreply.github.com>
2019-10-11 20:49:10 +0200

    11/10_3

--- FundracingProject/src/esercizio1/DepositoDati.java
+++ FundracingProject/src/esercizio1/DepositoDati.java
@@ -36,20 +36,21 @@ public class DepositoDati {
 	}
 	
 	public List<RowTableProjects> getProjects(String agencyName){
-		String sqlStr = "select	p.id as id_project, p.nome, p.budget, f.budget as stake, f.azienda\r\n" + 
+		String sqlStr = "select	f.progetto as id_project, p.nome, p.budget, sum(f.budget) as stake, f.azienda, (sum(f.budget)/p.budget)*100 as progress\r\n" + 
 				"from	progetto as p\r\n" + 
 				"		inner join\r\n" + 
 				"        finanziamento as f\r\n" + 
 				"        on p.id = f.progetto\r\n" +
 				"where p.azienda = (?)\r\n" +
-				"order by p.id;";
-		String sqlStr2 = "select	p.id as id_project, p.nome, p.budget, f.budget as stake, f.azienda\r\n" + 
+				"group by f.progetto;";
+		
+		String sqlStr2 = "select	f.progetto as id_project, p.nome, p.budget, sum(f.budget) as stake, f.azienda, (sum(f.budget)/p.budget)*100 as progress\r\n" + 
 				"from	progetto as p\r\n" + 
 				"		inner join\r\n" + 
 				"        finanziamento as f\r\n" + 
 				"        on p.id = f.progetto\r\n" +
 				"where p.azienda != (?)\r\n" +
-				"order by p.id;";
+				"group by f.progetto, f.azienda;";
 		
 		List<RowTableProjects> ret = new ArrayList<RowTableProjects>();
 		
@@ -63,12 +64,12 @@ public class DepositoDati {
 			ResultSet rs2 = pstm2.executeQuery();
 			
 			while(rs.next()) {
-				ret.add(new RowTableProjects(rs.getInt("id_project"), rs.getString("nome"), "ciao", rs.getInt("budget"), rs.getInt("stake"), rs.getString("azienda")));
+				ret.add(new RowTableProjects(rs.getInt("id_project"), rs.getString("nome"), rs.getString("progress"), rs.getInt("budget"), rs.getInt("stake"), rs.getString("azienda")));
 				//System.out.println(rs.getInt("id_project"));
 			}
 			
 			while(rs2.next()) {
-				ret.add(new RowTableProjects(rs2.getInt("id_project"), rs2.getString("nome"), "ciao", rs2.getInt("budget"), rs2.getInt("stake"), rs2.getString("azienda")));
+				ret.add(new RowTableProjects(rs2.getInt("id_project"), rs2.getString("nome"), rs2.getString("progress"), rs2.getInt("budget"), rs2.getInt("stake"), rs2.getString("azienda")));
 			}
 		}catch(SQLException e) {
 			System.out.println(e.getMessage());
